b75aa0f60af1ceca190944acc4423726c5c4cf03
Cxxshyy <dj.catlyn@gmail.com>
2021-03-05 21:19:51 +0000

    fixed grammar mistakes and added a link to udemy course

--- source/src/pages/about.js
+++ source/src/pages/about.js
@@ -2,24 +2,24 @@ import React from "react";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 function About() {
-	return (
-		<Layout title="Hello">
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					height: "50vh",
-					fontSize: "20px",
-				}}
-			>
-				<p>
-					<b>Coming soon...</b> <br></br>For now check out{" "}
-					<Link to={"/docs/"}>Our Docs!</Link>
-				</p>
-			</div>
-		</Layout>
-	);
+  return (
+    <Layout title="Hello">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "50vh",
+          fontSize: "20px",
+        }}
+      >
+        <p>
+          <b>Coming soon...</b> <br></br>For now check out{" "}
+          <Link to={"/docs/"}>Our Docs!</Link>
+        </p>
+      </div>
+    </Layout>
+  );
 }
 
 export default About;
